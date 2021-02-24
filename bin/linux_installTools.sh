5528a3ab3d8df307c11dc20fb4a7e540851da307
brian_hy <bghyams@gmail.com>
2021-02-24 08:01:49 -0600

    Update setup-env based on last couple years

---
+++ bin/linux_installTools.sh
@@ -0,0 +1,9 @@
+echo "Linux install tools"
+
+echo "Installing tree..."
+sudo apt-get install tree
+
+echo "Installing GNU parallel..."
+sudo apt-get install parallel
+
+echo "Done installing tools"
